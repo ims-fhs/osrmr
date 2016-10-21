0f9963f606b608e664836b332b8b09b3c48b4719
Christoph Strauss <christoph.strauss@fhsg.ch>
2016-10-21 13:07:59 +0200

    DESCRIPTION: Use depends, NAMESPACE via roxygen, update documentation, add test for decoder.

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,8 +1,9 @@
+# .......................................... integrate in functions for OS-independecy? What is state of the art?
 if (Sys.info()[[1]] == "Windows") {
   plattform <- "windows" # or "mac"
 } # ............................................................................ Add result MAC from SCN
 
 if (plattform == "windows") {
-  osrm_path <- "C:/OSRM/" # or "/Users/adrianschmid/Documents/OSRM/osrm-backend/build/"
+  osrm_path <- "C:/OSRM/" # or "/Users/adrianschmid/Documents/OSRM/osrm-backend/build/" => Needs to be an argument
 } # ............................................................................ Add result MAC from SCN
 
--- tests/testthat/test-basic_functions.R
+++ tests/testthat/test-basic_functions.R
@@ -26,3 +26,17 @@ test_that("viaroute finds routes - Either (V5 + web) or (V4 + server)", {
   quit_server()
 })
 
+context("decoder")
+
+test_that("decoder for consistent results", {
+  # skip("Skip for development of tests")
+
+  encoded_str <-"q|x}Gkuuq@u@Vw@ReAT_AJ}@Bs@CgAQgA[gAi@mAcAsDgDkByA}@k@_Ae@oAk@Ac@gAa@yAc@o@Qe@M}@Uo@IkAOsAGk@Bm@Fo@Js@RiAh@o@^s@h@_A|@_BhBuAhByA~BkHlMoCpDoAp@kA\\eCN}C?{D^_NdEoBr@uGtByIfD{@p@_A|@cAxAo@rAGHa@p@k@lAaBxDi@|@o@v@kAtAc@p@a@v@OPOFM?MMGMGM@EDO@UE]IQKIKCEAI@MHIJEPCT@PBPLTHDB@P@BAZ`@VXTNXHN?\\MTUp@_An@{@Zg@Tk@^cAlAsC^yABGfAwCz@yA|@mAz@u@v@s@rIeDtGuB|Bw@nMyDvDc@`DDfCOpAg@jAs@rCuDfHqMdBgCnAcBr@}@fAiAz@u@z@g@j@[v@Wv@Q^Eh@Id@Ab@?`@Bh@Dx@L`@H`B^l@RrC|@dA^~Ap@`Aj@t@f@hBtAhD~Ct@n@t@h@f@Xl@Tf@Pp@JhAFjA?bAKz@Q|@UjBo@j@Q`A[v@OdAM|@A|@BvBNbFh@tEf@~CXpCVxCPzD\\r@B`@?f@Ch@Gf@Ip@Sf@Sh@Yj@c@x@{@`AmAnBeCh@eAjAkC~EmI~@oCf@uB\\iBb@mDTuCPeJ_@qYOqCWgCa@gCq@cDk@iCi@sBa@iAi@aBY_AS_AQuAIuAC{A@uA@}ABuAH{ABc@Hw@RsARoAXuAXkA`@{ARq@Zy@Zs@\\s@h@_AlA_BxB{ChBgCzJaNrAiBlBkCtB}ClAgBfAaBpCuExAsCjA_CdAeC~@aC~@gCb@qAf@aBd@eBd@cB\\yA\\{@|@}DRkAVmAXoAXsB|@_HJg@Tk@V[VIHPLHL?HCJIDKBO@SCQISJSNe@H_@Dc@JwB@y@C[CYt@a@RMlBw@|Aq@XOBFBDFBD@DADCHd@TpA"
+  # encoded <- strwrap(encoded_str, width=10000, simplify=T)
+
+  geometry <- decode_geom(encoded_str)
+  expect_equal(all(dim(geometry) == c(281, 2)), T)
+  expect_equal(geometry$lat[1], 46.99097)
+  expect_equal(geometry$lng[1], 8.30822)
+})
+

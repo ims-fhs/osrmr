5412b6d4f0cdc1d97901cd1a5db28ca082af0961
Adrian Schmid <trompetenschmid@gmail.com>
2016-11-24 15:37:47 +0100

    Updated Documentation

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,5 +1,5 @@
 if (.Platform$OS.type == "windows") {
   osrm_path <- "C:/OSRM/"
 } else {
-    osrm_path <- "/Users/adrianschmid/Documents/OSRM/osrm-backend/build/"
+  osrm_path <- "/Users/adrianschmid/Documents/OSRM/osrm-backend/build/"
 }

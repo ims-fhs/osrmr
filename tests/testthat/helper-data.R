79666e0c2d1cc6ed9516798dc9eb7df0cd0d944c
Christoph Strauss <christoph.strauss@fhsg.ch>
2017-10-24 17:42:04 +0200

    Make test run again. Parameters api_version and localhost go to helper-data, add run_server() and quit_server() inside EACH test.

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,1 +1,3 @@
 osrm_path <- "C:/OSRM_API5/"
+api_version <- 5
+localhost <- TRUE
--- tests/testthat/test-Y-microbenchmark.R
+++ tests/testthat/test-Y-microbenchmark.R
@@ -8,8 +8,9 @@ test_that("microbenchmark test for osrm package", {
   lat2 <- 46.978
   lng2 <- 8.335
 
+  run_server(osrm_path)
   t1 <- summary(microbenchmark::microbenchmark(
-    osrmr::viaroute(lat1, lng1, lat2, lng2, F, 5, F),
+    osrmr::viaroute(lat1, lng1, lat2, lng2, F, api_version, localhost),
     times = 50L, unit = "ms"))$uq
   expect_equal(t1 < 400, T)
   # print(paste0("osrm_viaroute needs: ", round(t1, 1), " ms."))
@@ -19,9 +20,6 @@ test_that("microbenchmark test for osrm package", {
   # osrm_viaroute_fast needs: 115.9 ms.
   # osrm_viaroute needs: 116.4 ms.
 
-  # without message:
-  # viaroute_fast web needs: 107.7 ms.
-  # viaroute V5 web needs: 107.7 ms.
-  # viaroute V4 local needs: 11.8 ms.
+  quit_server()
 })
 

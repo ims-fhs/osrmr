c303a1a8cd928239b9b0f2297c60b28032ec2b25
Michael Schmid <michael.schmid@fhsg.ch>
2017-12-20 10:10:31 +0100

    ...see last commit. Adaption of test and man for decode_geom()

--- tests/testthat/test-basic_functions.R
+++ tests/testthat/test-basic_functions.R
@@ -45,13 +45,13 @@ test_that("decoder for consistent results", {
   encoded_str <-"q|x}Gkuuq@u@Vw@ReAT_AJ}@Bs@CgAQgA[gAi@mAcAsDgDkByA}@k@_Ae@oAk@Ac@gAa@yAc@o@Qe@M}@Uo@IkAOsAGk@Bm@Fo@Js@RiAh@o@^s@h@_A|@_BhBuAhByA~BkHlMoCpDoAp@kA\\eCN}C?{D^_NdEoBr@uGtByIfD{@p@_A|@cAxAo@rAGHa@p@k@lAaBxDi@|@o@v@kAtAc@p@a@v@OPOFM?MMGMGM@EDO@UE]IQKIKCEAI@MHIJEPCT@PBPLTHDB@P@BAZ`@VXTNXHN?\\MTUp@_An@{@Zg@Tk@^cAlAsC^yABGfAwCz@yA|@mAz@u@v@s@rIeDtGuB|Bw@nMyDvDc@`DDfCOpAg@jAs@rCuDfHqMdBgCnAcBr@}@fAiAz@u@z@g@j@[v@Wv@Q^Eh@Id@Ab@?`@Bh@Dx@L`@H`B^l@RrC|@dA^~Ap@`Aj@t@f@hBtAhD~Ct@n@t@h@f@Xl@Tf@Pp@JhAFjA?bAKz@Q|@UjBo@j@Q`A[v@OdAM|@A|@BvBNbFh@tEf@~CXpCVxCPzD\\r@B`@?f@Ch@Gf@Ip@Sf@Sh@Yj@c@x@{@`AmAnBeCh@eAjAkC~EmI~@oCf@uB\\iBb@mDTuCPeJ_@qYOqCWgCa@gCq@cDk@iCi@sBa@iAi@aBY_AS_AQuAIuAC{A@uA@}ABuAH{ABc@Hw@RsARoAXuAXkA`@{ARq@Zy@Zs@\\s@h@_AlA_BxB{ChBgCzJaNrAiBlBkCtB}ClAgBfAaBpCuExAsCjA_CdAeC~@aC~@gCb@qAf@aBd@eBd@cB\\yA\\{@|@}DRkAVmAXoAXsB|@_HJg@Tk@V[VIHPLHL?HCJIDKBO@SCQISJSNe@H_@Dc@JwB@y@C[CYt@a@RMlBw@|Aq@XOBFBDFBD@DADCHd@TpA"
   # encoded <- strwrap(encoded_str, width=10000, simplify=T)
 
-  geometry <- decode_geom(encoded_str, api_version = 4)
+  geometry <- decode_geom(encoded_str, precision = 5)
   expect_equal(all(dim(geometry) == c(281, 2)), T)
   expect_equal(geometry$lat[1], 46.99097)
   expect_equal(geometry$lng[1], 8.30822)
 
   encoded_str_api_5 <- encoded_string_api_5
-  geometry_api_5 <- decode_geom(encoded_str_api_5, api_version = 5)
+  geometry_api_5 <- decode_geom(encoded_str_api_5, precision = 6)
   expect_equal(all(dim(geometry_api_5) == c(35, 2)), T)
   expect_equal(geometry_api_5$lat[1], 47.100198)
   expect_equal(geometry_api_5$lng[1], 8.099703)

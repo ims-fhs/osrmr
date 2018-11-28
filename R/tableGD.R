bbf2b4df892760873962a69f9cf426b5b8f20c33
Gian Luca <GianLuca@GianLucas-MacBook-Pro.local>
2018-11-28 13:12:39 +0100

    Update tableGD Roxygen created

--- R/tableGD.R
+++ R/tableGD.R
@@ -1,10 +1,25 @@
-#tabel function
-#
-#coordinates <- "13.38,52.41;12.4,48.3;13.2,51.3"
-#n <- 3
-#
-#
-#
+#'duration of the fastest route between all pairs of supplied coordinates
+#'
+#'For given coordinates, table() calculates route informations using OSRM.
+#'OSRM choooses the nearest point which can be accessed by car for the coordinates.
+#'The coordinate-standard is WGS84.
+#'
+#' @param coordinates A character which contains the coordinates "lat1,lng1;lat2,lng2..."
+#' (-90 < lat < 90)
+#' (-180 < lng <180)
+#' @param n A numeric (the number of coordinates)
+#' @param localhost A logical(TRUE-localhost is used, FALSE=onlinehost is used)
+#'
+#' @return A data.frame with the duration of all pairs of supplied coordinates
+#' @export
+#'
+#' @examples
+#'
+#' points <- "9.382589,47.428731;9.240543,47.49642;9.252642,47.412704"
+#' number <- 3
+#'
+#' table(points,number,F)
+#'
 table <- function(coordinates, n, localhost){
 
 
@@ -25,9 +40,13 @@ table <- function(coordinates, n, localhost){
 
 
 
-  solution <- data.frame(t(goal$durations[[1]]))
+  solution <- data.frame(goal$durations[[1]])
   for (i in 2:n){
-    solution[i,] <- data.frame(t(goal$durations[[i]]))
+    solution[,i] <- data.frame(goal$durations[[i]])
+  }
+  for (i in 1:n){
+  colnames(solution)[i] <- paste("START",goal$sources[[i]]$name, sep=" ",NULL)
+  rownames(solution)[i] <- paste("END",goal$sources[[i]]$name, sep=" ",NULL)
   }
   return(solution)
 

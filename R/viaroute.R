c97b8d27deeb1630c2f63bf773b43248b28d151e
Michael Schmid <michael.schmid@fhsg.ch>
2019-01-14 09:25:12 +0100

    Error-handling with tryCatch. First Version to try the principle. Works, but it's risky to return any value when an error occured (especially for Routes.RData)

--- R/viaroute.R
+++ R/viaroute.R
@@ -149,29 +149,34 @@ viaroute_api_v5 <- function(lat1, lng1, lat2, lng2, instructions, address) {
                      "?overview=full", sep = "", NULL)
   }
   R.utils::withTimeout({
-    repeat {
-      res <- try(
-        route <- rjson::fromJSON(
-          file = request))
-      if (class(res) != "try-error") {
-        if (!is.null(res)) {
-          break # waytime found
-        } else {
-          stop("in sim911::osrm_viaroute: calculate nearest necessary?")
+      res <- tryCatch(
+        {
+          rjson::fromJSON(file = request)
+        },
+        error = function(cond) {
+          message("Error:  OSRM Onlinehost doesn't react -> Time set to 16min ")
+          # message(cond)
+          return(list(routes = list(list(duration = 16*60)), code = "Ok"))},
+        warning = function(cond) {
+          message("Warning: OSRM Onlinehost doesn't react -> Time set to 16min ")
+          # message(cond)
+          return(list(routes = list(list(duration = 16*60)), code = "Ok"))
         }
-      }
-    }
-  }, timeout = 1, onTimeout = "warning")
+        )},
+
+      timeout = 1, onTimeout = "warning")
+
   assertthat::assert_that(assertthat::is.number(res$routes[[1]]$duration))
 
   if (!instructions) {
     if (res$code == "Ok") {
       return(res$routes[[1]]$duration)
-    } else {
-      t_guess <- 16*60
-      warning("Route not found: ", paste(lat1, lng1, lat2, lng2, collapse = ", "),
-              ". Travel time set to ", t_guess/60 , " min.")
     }
+    # else {
+    #   t_guess <- 16*60
+    #   warning("Route not found: ", paste(lat1, lng1, lat2, lng2, collapse = ", "),
+    #           ". Travel time set to ", t_guess/60 , " min.")
+    # }
   } else {
     return(res)
   }

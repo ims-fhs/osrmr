8fee6e7a3939b8b3a4872c0db0d69bb5629ffdbb
Christoph Strauss <christoph.strauss@fhsg.ch>
2019-08-05 14:53:05 +0200

    Update docs. Set test to osrm_path "C:/OSRM_API5/", map_name "switzerland-latest.osrm" and localhost "TRUE". Only 0-time/distance is test for correct value.

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,4 +1,3 @@
 osrm_path <- "C:/OSRM_API5/"
 map_name <- "switzerland-latest.osrm"
-api_version <- 5
-localhost <- FALSE
+localhost <- TRUE
--- tests/testthat/test-basic_functions.R
+++ tests/testthat/test-basic_functions.R
@@ -10,7 +10,7 @@ test_that("viaroute finds routes", {
   prec <- 0.02
 
   if (localhost) {
-    run_server(osrm_path)
+    run_server(osrm_path = osrm_path, map_name = map_name)
   }
   r <- viaroute(lat1, lng1, lat2, lng2, localhost = TRUE, instructions = FALSE)
   expect_equal(class(r), "numeric")
@@ -18,6 +18,18 @@ test_that("viaroute finds routes", {
   r <- viaroute(lat1, lng1, lat2, lng2, localhost = TRUE, instructions = TRUE)
   expect_equal(class(r), "list")
   expect_equal(length(r), 3)
+
+  # The same location should work as well:
+  r <- viaroute(lat1, lng1, lat1, lng1, localhost = TRUE, instructions = FALSE)
+  expect_equal(class(r), "numeric")
+  expect_equal(length(r), 1)
+  expect_equal(r, 0)
+  r <- viaroute(lat1, lng1, lat1, lng1, localhost = TRUE, instructions = TRUE)
+  expect_equal(class(r), "list")
+  expect_equal(length(r), 3)
+  expect_equal(r$routes[[1]]$distance, 0)
+  expect_equal(r$routes[[1]]$duration, 0)
+
   if (localhost) {
     quit_server()
   }
